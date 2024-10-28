a379b19340e7473165f931c21a2329e5d3c81864
ahmed hamed <ahmedahmedhamedahmed0@gmail.com>
2024-10-28 06:56:36 +0300

    (1) clumped asserts together

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -37,9 +37,7 @@ class Server:
         return self.__dataset
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
-        assert isinstance(page, int)
-        assert isinstance(page_size, int)
-        assert page > 0
-        assert page_size > 0
+        assert isinstance(page, int) and isinstance(page_size, int)
+        assert page > 0 and page_size > 0
         start, end = index_range(page, page_size)
         return self.dataset()[start:end]
