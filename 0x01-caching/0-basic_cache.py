26745156fdcb47078202d02203469fb1d76cff1e
ahmed hamed <ahmedahmedhamedahmed0@gmail.com>
2024-10-29 19:55:44 +0300

    (0) removed class

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -1,18 +1,3 @@
-"""Basic Cache module
-"""
 BaseCaching = __import__('caching').BaseCaching
 
 
-class BasicCache(BaseCaching):
-
-    def put(self, key, item):
-        if key is None or item is None:
-            return
-        self.cache_data[key] = item
-
-    def get(self, key):
-        """default dict behaviour
-        """
-        if key is None:
-            return
-        return self.cache_data.get(key)
