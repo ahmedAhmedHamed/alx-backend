e07a774ea068c128e8096d95458c4cdfcbf65576
ahmed hamed <ahmedahmedhamedahmed0@gmail.com>
2024-10-29 19:50:47 +0300

    (0) no longer overriding init

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -10,12 +10,6 @@ class BasicCache(BaseCaching):
     Basic cache class
     """
 
-    def __init__(self):
-        """
-        just calls super init
-        """
-        super().__init__()
-
     def put(self, key, item):
         """
         default dict behaviour
