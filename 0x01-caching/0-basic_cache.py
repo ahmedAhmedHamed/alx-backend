1fc46c45977bfe8efd2b984b7039cc3de2e5fd59
ahmed hamed <ahmedahmedhamedahmed0@gmail.com>
2024-10-30 08:07:22 +0300

    (0) added docstrings

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -1,14 +1,27 @@
+#!/usr/bin/env python3
+"""
+basic cache module
+"""
 BaseCaching = __import__('base_caching').BaseCaching
 
 
 class BasicCache(BaseCaching):
+    """
+    basic cache class
+    """
 
     def put(self, key, item):
+        """
+        dict.put func
+        """
         if key is None or item is None:
             return
         self.cache_data[key] = item
 
     def get(self, key):
+        """
+        dict.get func
+        """
         if key is None:
             return
         return self.cache_data.get(key)
