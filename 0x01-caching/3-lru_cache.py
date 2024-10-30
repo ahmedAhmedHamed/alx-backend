b6b9f30f921b2d002df839627aa78a2d6d01543f
ahmed hamed <ahmedahmedhamedahmed0@gmail.com>
2024-10-30 09:24:06 +0300

    (3)

--- 0x01-caching/3-lru_cache.py
+++ 0x01-caching/3-lru_cache.py
@@ -14,19 +14,37 @@ class LRUCache(BaseCaching):
         super().__init__()
         self.lru = []
 
+    def key_accessed_in_lru(self, key):
+        for i in range(len(self.lru)):
+            if self.lru[i][0] == key:
+                self.lru[i][1] = 0
+                break
+
     def put(self, key, item):
         """
         dict.put func
         """
         if key is None or item is None:
             return
+        if self.cache_data.get(key) is None:
+            self.lru.append([key, 0])
+        else:
+            self.key_accessed_in_lru(key)
         self.cache_data[key] = item
         if len(self.cache_data) > self.MAX_ITEMS:
-            removed_key = self.lru.pop()
-            self.lru.append(key)
-            del self.cache_data[removed_key]
-            print(f"DISCARD: {removed_key}")
-        self.lru.append(key)
+            maximum = -1
+            maximum_idx = -1
+            maximum_key = None
+            for i in range(len(self.lru)):
+                if maximum < self.lru[i][1]:
+                    maximum = self.lru[i][1]
+                    maximum_idx = i
+                    maximum_key = self.lru[i][0]
+            self.lru.pop(maximum_idx)
+            del self.cache_data[maximum_key]
+            print(f"DISCARD: {maximum_key}")
+        for i in range(len(self.lru)):
+            self.lru[i][1] += 1
 
     def get(self, key):
         """
@@ -34,4 +52,5 @@ class LRUCache(BaseCaching):
         """
         if key is None:
             return
+        self.key_accessed_in_lru(key)
         return self.cache_data.get(key)
