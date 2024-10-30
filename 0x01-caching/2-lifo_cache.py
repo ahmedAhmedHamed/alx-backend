9e8b33741fd5fad9ad9ddcc7c8f6ce2a5943194d
ahmed hamed <ahmedahmedhamedahmed0@gmail.com>
2024-10-30 08:27:07 +0300

    (2)

---
+++ 0x01-caching/2-lifo_cache.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+"""
+basic cache module
+"""
+from base_caching import BaseCaching
+
+
+class LIFOCache(BaseCaching):
+    """
+    LIFOCache  class
+    """
+
+    def __init__(self):
+        super().__init__()
+        self.lifo = []
+
+    def put(self, key, item):
+        """
+        dict.put func
+        """
+        if key is None or item is None:
+            return
+        self.cache_data[key] = item
+        if len(self.cache_data) > self.MAX_ITEMS:
+            removed_key = self.lifo.pop()
+            self.lifo.append(key)
+            del self.cache_data[removed_key]
+            print(f"DISCARD: {removed_key}")
+        self.lifo.append(key)
+
+    def get(self, key):
+        """
+        dict.get func
+        """
+        if key is None:
+            return
+        return self.cache_data.get(key)
