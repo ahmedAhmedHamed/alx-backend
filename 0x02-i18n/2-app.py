37a04b2d1cc662095333d7b7b3d91af9aee7aa79
ahmed hamed <ahmedahmedhamedahmed0@gmail.com>
2024-11-05 19:11:05 +0200

    (2) removed unused function

---
+++ 0x02-i18n/2-app.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+""" initial app """
+from flask import Flask, render_template, request
+from flask_babel import Babel
+
+
+app = Flask(__name__)
+
+
+class Config:
+    """
+    Config class
+    """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app.config.from_object(Config)
+
+babel = Babel(app)
+
+
+@babel.localeselector
+def get_locale() -> str:
+    """
+    returns the locale given a request
+    """
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@app.route("/")
+def hello_world() -> str:
+    """ hello_world route """
+    return render_template('1-index.html')
+
+
+if __name__ == '__main__':
+    """ launches application """
+    app.run()
