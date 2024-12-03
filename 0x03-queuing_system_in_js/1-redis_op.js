5c744111bda0d3855ae4853fda7c83bd27249266
ahmed hamed <ahmedahmedhamedahmed0@gmail.com>
2024-12-03 07:56:04 +0200

    (2)

---
+++ 0x03-queuing_system_in_js/1-redis_op.js
@@ -0,0 +1,24 @@
+import  redis, { createClient } from 'redis';
+
+const client = createClient();
+
+client.on('connect', () => {
+  console.log('Redis client connected to the server');
+});
+
+client.on('error', (err) => {
+  console.error(`Redis client not connected to the server: ${err}`, err);
+});
+
+function setNewSchool(schoolName, value) {
+  client.set(schoolName, value, redis.print);
+}
+
+function displaySchoolValue(schoolName) {
+  client.get(schoolName, function(err, reply) {
+    console.log(reply);
+  });
+}
+displaySchoolValue('Holberton');
+setNewSchool('HolbertonSanFrancisco', '100');
+displaySchoolValue('HolbertonSanFrancisco');
