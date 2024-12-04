ba508fa9866f54bb0d331a80ed015c99a9f4bb03
ahmedAhmedHamed <124739282+ahmedAhmedHamed@users.noreply.github.com>
2024-12-05 00:12:55 +0200

    (9) until list_product, now with more attention

--- 0x03-queuing_system_in_js/9-stock.js
+++ 0x03-queuing_system_in_js/9-stock.js
@@ -1,9 +1,10 @@
 import express from 'express';
-import { createClient } from 'redis';
+import redis, { createClient } from 'redis';
 import util from "util";
 
 const client = createClient();
 const getAsync = util.promisify(client.get).bind(client);
+const setAsync = util.promisify(client.set).bind(client);
 const app = express();
 const PORT = 1245;
 
@@ -34,23 +35,45 @@ const listProducts = [
   },
 ]
 
+function reformat(input) {
+  const output = {}
+  output['itemId'] = input['id'];
+  output['itemName'] = input['name'];
+  output['price'] = input['price'];
+  output['initialAvailableQuantity'] = input['stock'];
+  return output;
+}
+
 function getItemById(id) {
   return listProducts.find(product => product.id === id) || null;
+}
+
+function reserveStockById(itemId, stock) {
+  return setAsync(`item.${itemId}`, stock);
+}
+
+async function getCurrentReservedStockById(itemId) {
+  return await getAsync(itemId) || 0;
 }
 
 app.listen(PORT);
 
 app.get('/list_products', (req, res)=>{
-  res.json(listProducts);
+  const ret = [];
+  listProducts.forEach((product) => {
+    ret.push(reformat(product));
+  });
+  res.json(ret);
 });
 
-app.get('/list_products/:itemId', (req, res) => {
+app.get('/list_products/:itemId', async (req, res) => {
   const itemId = parseInt(req.params.itemId, 10);
-  const product = getItemById(itemId);
-
-  if (product) {
-    res.json(product);
-  } else {
-    res.json({ status: 'Product not found' });
+  const item = getItemById(itemId);
+  const reserved = await getCurrentReservedStockById(itemId) || 0;
+  let ret = {"status":"Product not found"};
+  if (item) {
+    ret = reformat(item);
+    ret.currentQuantity = item.stock - reserved;
   }
+  res.json(ret);
 });
